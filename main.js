c34c359c5193e3a5f93d269d00feaa02a34801cf
Ethan Smith <ethan@onebytegone.com>
2015-03-20 22:18:04 -0400

    Initial code commit

---
+++ main.js
@@ -0,0 +1,11 @@
+require.config({
+    baseUrl: '',
+    paths: {
+      config: 'config/config',
+
+      backbone: 'thirdparty/backbone',
+      jquery: 'thirdparty/jquery-1.11.2.min',
+      rivets: 'thirdparty/rivets.bundled.min',
+      underscore: 'thirdparty/underscore-min'
+   }
+});
